00f291b40a907a1161c4a3b6f8a7e54d66619f59
Krushith Reddy Thoutireddy <Kthoutireddy5880@conestogac.on.ca>
2024-09-27 16:48:35 -0400

    Add CI/CD workflow for database deployment

---
+++ schema_changes_git.sql
@@ -0,0 +1,19 @@
+
+*******************************************************
+
+
+Implementing CI/CD Pipelines for Automated Database Deployment
+
+
+*********************************************************
+SQL Script for Schema Changes
+
+----schema_changes_git.sql
+
+CREATE TABLE students (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
+  age INT
+);
+
+*********************************************************
